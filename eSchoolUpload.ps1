cd75b816f258d865960747dbd29d7768107a249a
Cweb <xeniogame@gmail.com>
2020-01-31 15:06:30 -0600

    Update eSchoolUpload.ps1

--- eSchoolUpload.ps1
+++ eSchoolUpload.ps1
@@ -5,28 +5,31 @@
 #V1 Testing the flexible $currentyear to dynamically set the school year based off the current month.
 
 Param(
-[paramater(Position=0,mandatory=$false,Helpmessage="Optional year input will default to current school year")]
-[String]$CurrentYear = (IF((Get-date).month -le "6") {(Get-date).year} else {(Get-date).year+1}),
-[paramater(Position=1,mandatory=$true,Helpmessage="What file do you want to upload")]
-[String]$InFile = "C:\scripts\Mass-EmailUpdate-Eschool.csv", #***Variable*** Change to default upload file if you want to specify one
-[paramater(Position=2,mandatory=$false,Helpmessage="Eschool username")]
-[String] $username = "Schoolaccount", #***Variable*** Change to default eschool usename
-[paramater(Position=3,mandatory=$false,Helpmessage="Run mode, V for verfiy and R to commit data changes to eschool")][ValidateSet("R","V")]
-[String]$RunMode = "V",
-[paramater(Position=4,mandatory=$false,Helpmessage="Interface upload Definition to run")]
-[String]$InterfaceID, #**Variable** Default upload definition you want to call, can be found on the upload/download defintiion Interface ID
+[parameter(Position=0,mandatory=$true,Helpmessage="Optional year input will default to current school year")]
+$CurrentYear,
+[parameter(Position=1,mandatory=$false,Helpmessage="What file do you want to upload, Full path c:\scripts\filename.csv")]
+[String]$InFile = "C:\scripts\Mass-EmailUpdate-Eschool3.csv", #***Variable*** Change to default upload file if you want to specify one
+[parameter(Position=2,mandatory=$false,Helpmessage="Eschool username")]
+[String] $username="SSOusername", #***Variable*** Change to default eschool usename
+[parameter(Position=3,mandatory=$false,Helpmessage="Run mode, V for verfiy and R to commit data changes to eschool")][ValidateSet("R","V")]
+[String]$RunMode="V",
+[parameter(Position=4,mandatory=$false,Helpmessage="Interface upload Definition to run")]
+[String]$InterfaceID, #**Variable** Default upload definition you want to call, can be found on the upload/download defintiion Interface ID [CASE SENSTIVE!]
 [parameter(Mandatory=$false,HelpMessage="File for ADE SSO Password")]
-[string]$passwordfile="C:\Scripts\apscnpw.txt" #--- VARIABLE --- change to a file path for SSO password
+[string]$passwordfile="C:\Scripts\apscnpw.txt", #--- VARIABLE --- change to a file path for SSO password
+[parameter(Position=0,mandatory=$false,Helpmessage="Specify the time to wait before running the upload script")]
+[int]$addtime = "1" #Specify the time in minutes to wait to run the upload definition
 )
-
+Add-Type -AssemblyName System.Web
 If ((Test-Path ($passwordfile))) {
-    $password = Get-Content $passwordfile | ConvertTo-SecureString
+    $password = Get-Content $passwordfile | ConvertTo-SecureString -AsPlainText -Force
 }
 Else {
     Write-Host("Password file does not exist! [$passwordfile]. Please enter a password to be saved on this computer for scripts") -ForeGroundColor Yellow
     Read-Host "Enter Password" -AsSecureString |  ConvertFrom-SecureString | Out-File $passwordfile
-    $password = Get-Content $passwordfile | ConvertTo-SecureString
+    $password = Get-Content $passwordfile | ConvertTo-SecureString -AsPlainText -Force
 }
+$password = "Sadlyunsecuredpasswordsfornow"
 # ---------------------------------------------------------------------------------------------------------------------
 # Various URL variables.  Up top for when SunGard inevitably changes them all... --------------------------------------
 $baseUrl = "https://eschool40.esp.k12.ar.us/eSchoolPLUS40/"
@@ -48,8 +51,15 @@ $form.Fields["UserName"] = $username
 $form.Fields["Password"] = $password
 
 #Login to eSchool with the created session (which now includes username/password)
-$response2 = Invoke-WebRequest -Uri $loginUrl -WebSession $rb -Method POST -Body $form.Fields
-
+Try{
+$response2 = Invoke-WebRequest -Uri $loginUrl -WebSession $rb -Method POST -Body $form.Fields -ErrorAction Stop
+$statuscode = $response2.Statuscode
+}
+Catch
+{
+ $statuscode = $_.Exception.Response.Statuscode.value__
+}
+$statuscode
 #Get the first HTML <form> element on the page (set environment form in this case)
 $form2 = $response2.Forms[0]
 
@@ -60,7 +70,7 @@ $form2 = $response2.Forms[0]
 
 $form2.Fields["EnvironmentConfiguration.SchoolYear"] = $CurrentYear #change for alternate years' databases
 $form2.Fields["EnvironmentConfiguration.SummerSchool"] = "false" #"not supported in AR at this time"
-$form2.Fields["EnvironmentConfiguration.Database"] = "2360" #not sure what this does, but the form will not submit without it
+$form2.Fields["EnvironmentConfiguration.Database"] = "2110" #not sure what this does, but the form will not submit without it
 $form2.Fields["EnvironmentConfiguration.ImpersonatedUser"] = "" 
 
 #Pass the environment setting screen to finish logging in
@@ -81,12 +91,15 @@ $fileName = Split-Path $InFile -leaf
 $enc = [System.Text.Encoding]::GetEncoding("iso-8859-1")
 $boundary = [guid]::NewGuid().ToString()
 $fileBin = [System.IO.File]::ReadAllBytes($InFile)
+#Don't mess with spacing till after the last '@ it matters!
 $template = @'
 --{0}
 Content-Disposition: form-data; name="fileData"; filename="{1}"
 Content-Type: {2}
+
 {3}
 --{0}--
+
 '@
 
 $body = $template -f $boundary, $fileName, $mimeType, $enc.GetString($fileBin)
@@ -99,6 +112,8 @@ $response5 = Invoke-WebRequest -Uri $uploadUrl -Method Post -WebSession $rb -Con
 #Below is specific to an Upload Interface I've made in eSchool.  You'll either have to modify it to match what you're doing, or comment out here below.
 #You can schedule eSchool upload interfaces to run on a schedule within eSchool, if you don't want to mess with this.
 #-----------------------------------------------------------------------------------------------------------------------------
+
+If ($InterfaceID){
 
 #Navigate to Run Upload page
 $response6 = Invoke-WebRequest -Uri $runuploadUrl -WebSession $rb -Method GET
@@ -133,9 +148,9 @@ $form4.Fields["TaskScheduler.CurrentTask.Saturday"] = "false"
 $form4.Fields["TaskScheduler.CurrentTask.SchdInterval"] = "1"
 $form4.Fields["TaskScheduler.CurrentTask.ScheduleType"] = "N"
 $form4.Fields["TaskScheduler.CurrentTask.ScheduledTimeDate"] = get-date -UFormat %m/%d/%Y #"05/07/2019"
-$form4.Fields["TaskScheduler.CurrentTask.ScheduledTimeTime"] = (get-date).AddMinutes(1).ToString("hh:mm tt") #Set forward 1 minute(s) "03:45 PM"
+$form4.Fields["TaskScheduler.CurrentTask.ScheduledTimeTime"] = (get-date).AddMinutes($addtime).ToString("hh:mm tt") #Set forward 1 minute(s) "03:45 PM"
 $form4.Fields["TaskScheduler.CurrentTask.Sunday"] = "false"
-$form4.Fields["TaskScheduler.CurrentTask.TaskDescription"] = "Run Interface Upload (automated, AKA Ben Rules)"
+$form4.Fields["TaskScheduler.CurrentTask.TaskDescription"] = "Run Interface Upload (automated, AKA Ben Janelle is our hero all praise BEN)"
 $form4.Fields["TaskScheduler.CurrentTask.Thursday"] = "false"
 $form4.Fields["TaskScheduler.CurrentTask.Tuesday"] = "false"
 $form4.Fields["TaskScheduler.CurrentTask.Wednesday"] = "false"
@@ -146,3 +161,8 @@ $form4.Fields["tableKey"] = ""
 
 #Submit the Run Upload form with set variables
 $response7 = Invoke-WebRequest -Uri $runuploadUrl -WebSession $rb -Method POST -Body $form4.Fields
+Write-Host "Upload finished and task has been scheduled to run in $addtime minutes" -ForegroundColor Cyan -BackgroundColor Black
+}
+else 
+{Write-Host "Upload has finished to Eschool" -ForegroundColor Yellow -BackgroundColor Black
+}
